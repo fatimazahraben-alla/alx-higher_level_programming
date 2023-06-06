88917641df9bad0724f75bc4885cc824789b7f71
fatimazahraben-alla <fatisafae33@gmail.com>
2023-06-06 15:14:38 -0700

    Add 1-last_digit.py and 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-l_digit = number % 10
+l_digit = abs(number % 10)
+if number < 0:
+    l_digit *= -1
 if l_digit > 5:
     print(f"Last digit of {number} is {l_digit} and is greater than 5")
 elif l_digit < 6 and l_digit != 0:
