771738e4850492350b7d6b5e6f940f5f5d856e61
fatimazahraben-alla <fatisafae33@gmail.com>
2023-06-06 17:12:30 -0700

    Fix 6-print_comb3.py and 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        c = chr(ord(c) - 32)
+        if ord('a') <= ord(c) <= ord('z'):
+            c = chr(ord(c) - 32)
         print("{}".format(c), end='')
     print("")
