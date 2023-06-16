56bdeb7460468d84289425945bdb3a74135c0697
fatimazahraben-alla <fatisafae33@gmail.com>
2023-06-16 11:24:31 -0700

    upd 7-update_dictionary.py and 8-simple_delete.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,17 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    return {**a_dictionary, key: value}
-
-if __name__ == '__main__':
-    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-    new_dict = update_dictionary(a_dictionary, 'language', "Python")
-    print(sorted(new_dict.items()))
-    print("--")
-    print(sorted(a_dictionary.items()))
-    print("--")
-
-    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-    print(sorted(new_dict.items()))
-    print("--")
-    print(sorted(a_dictionary.items()))
-    print("--")
+    a_dictionary[key] = value
+    return a_dictionary
