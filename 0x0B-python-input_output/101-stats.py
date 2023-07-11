6923012c44659428b6e4f017db10cc22696886a0
fatimazahraben-alla <fatisafae33@gmail.com>
2023-07-11 13:14:19 -0700

    fix 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -4,16 +4,17 @@
 A script that reads stdin line by line and computes metrics
 """
 
+
 import sys
 
 
 def print_stats(status_codes, file_size):
     """
-    prints stats
+    Prints statistics including file size and status code counts.
 
     Arguments:
-        status_codes {dict} -- [dictionary of status codes]
-        file_size {int} -- [file size]
+        status_codes {dict} -- A dictionary of status codes and their counts
+        file_size {int} -- The total file size
     """
     print("File size: {}".format(file_size))
     for key, value in sorted(status_codes.items()):
@@ -33,11 +34,11 @@ if __name__ == "__main__":
             split_line = line.split()
             try:
                 file_size += int(split_line[-1])
-            except [ValueError, IndexError]:
+            except (ValueError, IndexError):
                 pass
             try:
                 status_codes[split_line[-2]] += 1
-            except [KeyError, IndexError]:
+            except (KeyError, IndexError):
                 pass
             if count % 10 == 0:
                 print_stats(status_codes, file_size)
