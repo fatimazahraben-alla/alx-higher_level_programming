3320d8f76d228dca0903060add9d81f227d2d84d
fatimazahraben-alla <fatisafae33@gmail.com>
2023-08-15 16:26:16 -0700

    UPD 9-full_creation.sql file

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS second_table (
 	id    INT,
 	name  VARCHAR(256),
 	score INT);
-INSERT INTO second_table VALUES (id = 1, name = "John", score = 10);
-INSERT INTO second_table VALUES (id = 2, name = "Alex", score = 3);
-INSERT INTO second_table VALUES (id = 3, name = "Bob", score = 14);
-INSERT INTO second_table VALUES (id = 4, name = "George", score = 8);
+INSERT INTO second_table
+VALUES  (1, "John", 10),
+	(2, "Alex", 3),
+	(3, "Bob", 14),
+	(4, "George", 8);
