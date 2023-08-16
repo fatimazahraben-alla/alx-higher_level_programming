5fcfe8e1e86ccddde22ba172834ebf37cde4c0d5
fatimazahraben-alla <fatisafae33@gmail.com>
2023-08-15 17:08:57 -0700

    UPD 16-no_link.sql file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- script that lists all records of the table second_table of the database hbtn_0c_0
 SELECT score, name
 FROM second_table
-WHERE name != NULL
+WHERE name != "" AND name IS NOT NULL
 ORDER BY score DESC;
