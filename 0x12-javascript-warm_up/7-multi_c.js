b1368877853c17599699a26a22e2a28c850251ec
fatimazahraben-alla <fatisafae33@gmail.com>
2023-09-11 16:53:38 -0700

    fixed

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numOccurrences = parseInt(process.argv[2]);
+if (isNaN(num)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log("C is fun");
+  }
+}
