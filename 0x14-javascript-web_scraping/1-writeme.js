5494d4fb33593c53e03c92bc16deddbab1dbe608
fatimazahraben-alla <fatisafae33@gmail.com>
2023-10-24 20:52:30 +0000

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+//script that writes a string to a file
+const fs = require('fs');
+const filename = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(filename, content, 'utf-8', (error) => {
+  if (error) {
+    console.log(error);
+  }
+});
